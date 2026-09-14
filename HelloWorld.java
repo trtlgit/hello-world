ceff8f81cad6bcebd5999a71726a0b8b24f801b5
Arabrate <cedric.caviezel@icloud.com>
2026-09-14 09:10:13 +0200

    getter hinzugefügt

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,13 @@
 public class HelloWorld {
 
-    public HelloWorld() {
+    private int i;
 
+    public HelloWorld() {
+        i = 0;
     }
+
+    public int getI() {
+        return i;
+    }
+
 }
